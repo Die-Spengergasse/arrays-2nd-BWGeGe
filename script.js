6956d43b0189e6c7e97d799ab3fad3899e710caa
BWGEGE <brz210319@spengergasse.at>
2023-12-04 21:40:56 +0100

    Finished

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
 const input3 = document.getElementById("input3");
+const input4 = document.getElementById("input4");
 
 const output = document.getElementById("output");
 output.publish = function (arg) {
@@ -11,12 +12,120 @@ const button_push = document.getElementById("button_push");
 button_push.addEventListener("click", push);
 
 const button_indexof = document.getElementById("button_indexof");
+button_indexof.addEventListener("click", indexof);
+
 const button_includes = document.getElementById("button_includes");
+button_includes.addEventListener("click", includes);
+
 const button_slice = document.getElementById("button_slice");
+button_slice.addEventListener("click", slice);
+
+const button_of = document.getElementById("button_of");
+button_of.addEventListener("click", of);
+
+const button_splice = document.getElementById("button_splice");
+button_splice.addEventListener("click", splice);
+
+const button_map = document.getElementById("button_map");
+button_map.addEventListener("click", map);
+
+const button_filter = document.getElementById("button_filter");
+button_filter.addEventListener("click", filter);
+
+const button_split = document.getElementById("button_split");
+button_split.addEventListener("click", split);
+
+const button_join = document.getElementById("button_join");
+button_join.addEventListener("click", join);
+
+const button_some = document.getElementById("button_some");
+button_some.addEventListener("click", some);
+
+const button_reverse = document.getElementById("button_reverse");
+button_reverse.addEventListener("click", reverse);
+
+const button_spread = document.getElementById("button_spread");
+button_spread.addEventListener("click", spread);
+
+const button_reduce = document.getElementById("button_reduce");
+button_reduce.addEventListener("click", reduce);
 
 function push() {
     const i1 = eval(input1.value);
     const i2 = eval(input2.value);
     i1.push(i2);
     output.publish(i1);
+}
+function indexof() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    
+    output.publish(i1.indexOf(i2));
+}
+function includes() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    output.publish(i1.includes(i2));
+}
+function slice() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    const i3 = eval(input3.value);
+    output.publish(i1.slice(i2, i3));
+}
+function of() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    output.publish(Array.of(i1 , i2));
+}
+function splice() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    const i3 = eval(input3.value);
+    const i4 = eval(input4.value);
+    output.publish(i1.splice(i2, i3, i4));
+}
+function map() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    output.publish(i1.map((x)=>x * i2));
+}
+function filter() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    output.publish(i1.filter((x)=>x == i2));
+}
+function split() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    output.publish(i1.split(i2));
+}
+
+function join() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    output.publish(i1.join(i2));
+}
+
+function some() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    output.publish(i1.some((x) => x == i2));
+}
+
+function reverse() {
+    const i1 = eval(input1.value);
+    output.publish(i1.reverse());
+}
+
+function spread() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    output.publish(...i1,...i2);
+}
+
+function reduce() {
+    const i1 = eval(input1.value);
+    const i2 = eval(input2.value);
+    output.publish(i1.reduce((acc, curr) => acc + curr, i2));
 }
